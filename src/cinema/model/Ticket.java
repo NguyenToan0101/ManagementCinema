04af6bd4af3708b7ecaf35f84405078d39bb953e
ADMIN <ADMIN@LAPTOP-G72J16G9>
2024-07-05 17:36:47 +0700

    Create ticket class

--- src/cinema/model/Ticket.java
+++ src/cinema/model/Ticket.java
@@ -4,10 +4,71 @@
  */
 package cinema.model;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 /**
  *
  * @author ADMIN
  */
 public class Ticket {
+    private String customerName;
+    private LocalDateTime purchaseDay;
+    private long ticketPrice;
+    private ShowtimesAndAuditorium showtimesAndAuditorium;
+    private ArrayList<Seat> seats;
+
+    public Ticket(String customerName, LocalDateTime purchaseDay, long ticketPrice, ShowtimesAndAuditorium showtimesAndAuditorium, ArrayList<Seat> seats) {
+        this.customerName = customerName;
+        this.purchaseDay = purchaseDay;
+        this.ticketPrice = ticketPrice;
+        this.showtimesAndAuditorium = showtimesAndAuditorium;
+        this.seats = seats;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public LocalDateTime getPurchaseDay() {
+        return purchaseDay;
+    }
+
+    public void setPurchaseDay(LocalDateTime purchaseDay) {
+        this.purchaseDay = purchaseDay;
+    }
+
+    public long getTicketPrice() {
+        return ticketPrice;
+    }
+
+    public void setTicketPrice(long ticketPrice) {
+        this.ticketPrice = ticketPrice;
+    }
+
+    public ShowtimesAndAuditorium getShowtimesAndAuditorium() {
+        return showtimesAndAuditorium;
+    }
+
+    public void setShowtimesAndAuditorium(ShowtimesAndAuditorium showtimesAndAuditorium) {
+        this.showtimesAndAuditorium = showtimesAndAuditorium;
+    }
+
+    public ArrayList<Seat> getSeats() {
+        return seats;
+    }
+
+    public void setSeats(ArrayList<Seat> seats) {
+        this.seats = seats;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket{" + "customerName=" + customerName + ", purchaseDay=" + purchaseDay + ", ticketPrice=" + ticketPrice + ", showtimesAndAuditorium=" + showtimesAndAuditorium + ", seats=" + seats + '}';
+    }
     
 }
